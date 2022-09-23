9fda8a9890fbaf9d900e19a8187ee0165b6c3e99
jacobwmorris <jacobwmorris0@gmail.com>
2022-09-22 20:06:23 -0400

    Grid colors are now random, and darken with repeated mouse-overs

--- etch.js
+++ etch.js
@@ -4,13 +4,59 @@ const gridPxWidth = 800; //window.getComputedStyle(gridDiv).width
 const gridPxHeight = 800;
 
 
+function getRand(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function colorFromRgb(colorRgb) {
+    const colorStr = colorRgb.split(",", 3);
+    return [parseInt(colorStr[0]), parseInt(colorStr[1]), parseInt(colorStr[2])];
+}
+
+function darken(color, darkness) {
+    let darknessPercent = 1 - darkness / 10;
+    if (darknessPercent < 0) {
+        darknessPercent = 0;
+    }
+    if (darknessPercent > 1) {
+        darknessPercent = 1;
+    }
+
+    return color.map(value => Math.floor(value * darknessPercent));
+}
+
 function colorCell(event) {
-    event.target.classList.add("colored");
+    let cell = event.target;
+
+    //If this is the first pass, apply a random color
+    if (cell.getAttribute("data-color") === "none") {
+        const colorRgb = `${getRand(256)},${getRand(256)},${getRand(256)}`;
+        cell.setAttribute("data-color", colorRgb);
+        cell.style.backgroundColor = "rgb(" + colorRgb + ")";
+        return;
+    }
+
+    //If the darkness is already maxed, return
+    let darkness = parseInt(cell.getAttribute("data-darkness"));
+    if (darkness >= 10)
+        return;
+
+    //Increment and apply darkness
+    ++darkness;
+    const color = colorFromRgb(cell.getAttribute("data-color"));
+    const darkenedColor = darken(color, darkness);
+
+    cell.setAttribute("data-darkness", darkness.toString());
+    cell.style.backgroundColor = `rgb(${darkenedColor[0]},${darkenedColor[1]},${darkenedColor[2]})`;
+    
+    return;
 }
 
 function makeGridCell(cellWidth, cellHeight) {
     let cell = document.createElement("div");
     cell.setAttribute("style", `width: ${cellWidth}px; height: ${cellHeight}px;`);
+    cell.setAttribute("data-color", "none");
+    cell.setAttribute("data-darkness", "0");
     cell.classList.add("gridCell");
 
     cell.addEventListener("mouseenter", colorCell);
